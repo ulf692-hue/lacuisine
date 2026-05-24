d73a5660d0328fab62775e22db38c8c216022639
ulf692-hue <ulf692@gmail.com>
2026-05-24 18:16:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
-const CACHE = 'lacuisine-v1';
+const CACHE = 'lacuisine-v9';
 const ASSETS = ['./index.html','./manifest.json','./icon.svg'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
